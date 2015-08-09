d0ed08b885de055164304287a882594ab55328d1
Kiosk <kiosk@cs.huji.ac.il>
2015-08-09 16:50:50 +0300

    project

--- database_setup.py
+++ database_setup.py
@@ -3,7 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
-Base = declarative_base()
+
 
 #PLACE YOUR TABLE SETUP INFORMATION HERE
-
+Picture_Base= declarative_base()
+class Picture(Picture_Base):
+        __tablename__='picture'
+        id=Column(Integer, primary_key=True)
+        age= Column(String)
+        gender=Column(String)
+        nationality=Column(String)
+        #user=Column(String)
+        filename=Column(String)
+        
--- web_app.py
+++ web_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,random,request
 app = Flask(__name__)
 
 # SQLAlchemy stuff
@@ -6,7 +6,7 @@ app = Flask(__name__)
 #from sqlalchemy import create_engine
 #from sqlalchemy.orm import sessionmaker
 #engine = create_engine('sqlite:///crudlab.db')
-#Base.metadata.bind = engine
+#Base.metadata.create_all(engine) 
 #DBSession = sessionmaker(bind=engine)
 #session = DBSession()
 
@@ -20,7 +20,33 @@ def main():
 
 
 
+@app.route('/game',methods=['GET','POST'])
+def start_game():
+    pictures=session.query(Picture).all()
+    picture= random.choice(pictures)
+    if request.method=='GET':
+        return render_template("game.html",pictures=pictures)
+    else:
+        user_answer_age=request.form['age']
+        user_answer_nationality=request.form['nationality']
+        user_answer_gender=request.form['gender']
+        if user_answer_age= picture.age:
+            
+            if user_answer_nationality=picutre.age:
+                if user_answer_gender=picture.gender:
+                    return redirect(url_for('correct.html')
+                    
+        else: return redirect(url_for('wrong.html'))
 
-
+@app.route('/game/correct')
+def correct_submit():
+    return render_template("correct.html")
+@app.route('/game/wrong')
+def wrong_submit():
+    return render_template("wrong.html")
+@app.route('/')
+def go_home():
+    return render_template("main_page.html")                                    
+    
 if __name__ == '__main__':
     app.run(debug=True)
